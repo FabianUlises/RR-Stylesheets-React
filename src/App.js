9de56e6d0c4879c59866f9236ab549a13a012b12
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-05-02 13:50:02 -0700

    Created two components Child1 & Child2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import './App.css';
+import Child1 from './Child1';
+import Child2 from './Child2';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Child1 />
+
+      <Child2 />
     </div>
   );
 }
---
+++ src/Child2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Child2() {
+    return(
+        <div>
+            Child2
+        </div>
+    )
+}
